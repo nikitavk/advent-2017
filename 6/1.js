6536abf34b4ae83588f5e5bb50efbc3dfbc8ef40
nkulik0v <desu27desU27>
2017-12-06 11:47:42 +0300

    add day 6

---
+++ 6/1.js
@@ -0,0 +1,27 @@
+const banks = `0	5	10	0	11	14	13	4	11	8	8	7	1	4	12	11`.split('\t').map(e => +e);
+
+let configurationHistory = [];
+configurationHistory.push(banks + '');
+let cycle = 0;
+
+while (1) {
+    let i = banks.indexOf(Math.max(...banks));
+
+    let reloc = banks[i];
+    // console.log(banks, reloc, i);
+    banks[i] = 0;
+
+    while (reloc > 0) {
+        i++;
+        if (i > banks.length - 1) {
+            i = 0;
+        }
+        banks[i]++;
+        reloc--;
+    }
+    cycle++;
+    if (configurationHistory.includes(banks + '')) break;
+    configurationHistory.push(banks + '');
+}
+
+console.log(banks, cycle);
